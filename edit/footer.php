34ca80279afc007128e031b4942668e570db05d4
Dave Parker <dave.x.parker@gmail.com>
2008-04-25 22:21:37 +0000

    Edit php files.

--- edit/footer.php
+++ edit/footer.php
@@ -9,6 +9,8 @@
 <td>[<a href="list.php">List</a>]</td>
 <td>[<a href="add.php">Add</a>]</td>
 <td>[<a href="rename.php">Rename</a>]</td>
+<td>[<a href="import.php">Import</a>]</td>
+<td>[<a href="report.php">Report</a>]</td>
 </tr></table>
 
 <br>
--- edit/header.php
+++ edit/header.php
@@ -14,7 +14,6 @@
 <td>[<a href="add.php">Add</a>]</td>
 <td>[<a href="rename.php">Rename</a>]</td>
 <td>[<a href="import.php">Import</a>]</td>
-<td>[<a href="cats.php">Cats</a>]</td>
 <td>[<a href="report.php">Report</a>]</td>
 </tr></table>
 
