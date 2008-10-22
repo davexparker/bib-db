4f2ecfc19debaa909b3517d188b6636f56255816
Dave Parker <dave.x.parker@gmail.com>
2008-10-22 19:43:00 +0000

    Improved import instructions.

--- edit/import.php
+++ edit/import.php
@@ -2,8 +2,16 @@
 
 <?php require '../bib_stuff.php'; ?>
 
-<pre>
-~dxp/www/bib/bib2postgresql/bib2postgresql < ~dxp/newfile.bib
-</pre>
+To import from bibtex:
+
+<ul>
+<li> upload .bib file to <tt>~dxp/doc/bib</tt> on <tt>qav</tt>
+<li> (there are template .bib files (e.g. <tt>.InProceedings</tt>, <tt>.Book</tt>) in <tt>~dxp/doc/bib</tt>)
+<br><br>
+<li> <tt>ssh qav</tt>
+<li> <tt>~dxp/bib-db/bib2postgresql/bib2postgresql < ~dxp/doc/bib/newfile.bib</tt>
+<br><br>
+<li> now <a href="list.php">edit</a> the new database entry (to set categories etc.)
+</ul>
 
 <?php require 'footer.php'; ?>
