0ea061e4079f5d530a838e50b13a813419d72b69
Dave Parker <dave.x.parker@gmail.com>
2011-12-22 15:02:50 +0000

    Portability fix

--- edit/import.php
+++ edit/import.php
@@ -16,9 +16,9 @@ Use this form to import to the database from bibtex.
 
 <p>
 See e.g.
-<a href="http://qav.cs.ox.ac.uk/bibtex/.Article">.Article</a> and
-<a href="http://qav.cs.ox.ac.uk/bibtex/.InProceedings">.InProceedings</a> for template bibtex files.
-All existing bibtex files  are <a href="http://qav.cs.ox.ac.uk/bibtex/">here</a>.
+<a href="$bib_bibtex_url/.Article">.Article</a> and
+<a href="$bib_bibtex_url/bibtex/.InProceedings">.InProceedings</a> for template bibtex files.
+All existing bibtex files  are <a href="$bib_bibtex_url/bibtex/">here</a>.
 </p>
 
 <p>
