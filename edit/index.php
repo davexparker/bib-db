c390893af69a7ca9a04e272a4c371f7f8f16f15e
Dave Parker <dave.x.parker@gmail.com>
2012-07-28 14:41:52 +0000

    Upload bibtex option

--- edit/index.php
+++ edit/index.php
@@ -7,7 +7,7 @@
 <li><a href="add.php"><b>Add</b> new publication</a>
 <li><a href="import.php"><b>Import</b> new publication from bibtex</a>
 <li><a href="upload-paper.php">Upload <b>PDF</b> for publication</a>
-<li><a href="upload-bib.php">Upload <b>bibtex</b> for publication</a>
+<li><a href="upload-bibtex.php">Upload <b>bibtex</b> for publication</a>
 <li><a href="rename.php"><b>Rename</b> key for existing publication</a>
 <li><a href="cats.php">Edit <b>categories</b></a>
 </ul>
