f6d119df434c9871b1e6429bdc7cb876e2ccb245
Dave Parker <dave.x.parker@gmail.com>
2012-07-28 14:42:42 +0000

    Upload bibtex option

--- edit/upload-bibtex.php
+++ edit/upload-bibtex.php
@@ -11,7 +11,7 @@ if (!array_key_exists("bibfile", $_FILES)) {
 ?>
 
 <p>
-Use this form to upload a paper to
+Use this form to upload a bibtex file to
 <a href="$bib_bibtex_url">this area</a> of the server.
 </p>
 
