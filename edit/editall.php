a39fa4f32fb4df6467aa43d266682dfd9939dbb7
Dave Parker <dave.x.parker@gmail.com>
2014-01-25 13:59:49 +0000

    Update editall page.

--- edit/editall.php
+++ edit/editall.php
@@ -8,6 +8,39 @@ require '../bib_stuff.php';
 $key = extract_key_from_post_get();
 if (!$key) return;
 
+?>
+
+<table><tr><td valign="top">
+
+<?php
+
+// print contents sidebar
+$keylist = bib_db_query("SELECT key FROM bib_items ORDER BY key");
+$n = count($keylist);
+$k = -1;
+for ($i = 0; $i < $n; $i++) if ($keylist[$i] == $key) $k = $i;
+if ($k >= 0) {
+	echo "------<br>\n";
+	$i1 = max(0, $k-5);
+	$i2 = min($k+5, $n-1);
+	if ($i1 > 0) echo "[<a href=\"edit.php?key=".$keylist[0]."\">".$keylist[0]."</a>]<br>\n...<br>\n";
+	for ($i = $i1; $i < $k; $i++) {
+		echo "[<a href=\"edit.php?key=".$keylist[$i]."\">".$keylist[$i]."</a>]<br>\n";
+	}
+	echo "[<b>".$key."</b>]<br>\n";
+	for ($i = $k+1; $i <= $i2; $i++) {
+		echo "[<a href=\"edit.php?key=".$keylist[$i]."\">".$keylist[$i]."</a>]<br>\n";
+	}
+	if ($i2 < $n-1) echo "...<br>\n[<a href=\"edit.php?key=".$keylist[$n-1]."\">".$keylist[$n-1]."</a>]<br>\n";
+	echo "------<br>\n";
+}
+
+?>
+
+</td><td>&nbsp;</td><td class="alt2">&nbsp;</td><td>&nbsp;</td><td valign="top">
+
+<?php
+
 // determine action required
 $action = extract_var_from_post_get("action");
 
@@ -36,7 +69,9 @@ else {
 
 <input type="submit" value="Submit changes">
 
-<br><br>
+<br>
+
+<h3>Bibtex fields:</h3>
 
 <table>
 <tr><td><b>Key:</b></td><td><b><?php echo $item["key"]; ?></b></td></tr>
@@ -50,14 +85,43 @@ foreach ($bib_item_fields as $field) {
 ?>
 
 <tr><td>&nbsp;</td><td></td></tr>
+<tr><td><input type="submit" value="Submit changes"></td><td></td></tr>
+<tr><td>&nbsp;</td><td></td></tr>
+
+</table>
+
+<h3>Other database fields:</h3>
+
+<table>
+
+<tr><td colspan="2">For local publications, enter the name of the PDF/PS file (without the extension):<br>
+Use e.g. <em>cav06</em> or <em>cav06symm</em> (for conferences), <em>fmsd-games</em> (for journals) or <em>RR-11-11</em> (for CS tech reports)</td></tr>
+<tr><td><b>Filename: </b></td><td><input type="text" size="100" name="filename" value="<?php echo $item["filename"]; ?>"></td></tr>
+<tr><td colspan="2">For other people's publications, enter the URL where it can be found:</td></tr>
+<tr><td><b>URL: </b></td><td><input type="text" size="100" name="url" value="<?php echo $item["url"]; ?>"></td></tr>
+
+<tr><td>&nbsp;</td></tr>
+
+<tr><td valign="top"><b>Abstract: </b><br>(html)</td><td><textarea rows="10" cols="70" name="abstract"><?php echo $item["abstract"]; ?></textarea></td></tr>
+<tr><td valign="top"><b>Notes: </b><br>(html)</td><td><textarea rows="5" cols="70" name="links"><?php echo $item["links"]; ?></textarea><br>
+Use e.g. "See [KNP06c]" to automatically get a link to another paper in the database.</td></tr>
+<tr><td valign="top"><b>Synopsis: </b></td><td><textarea rows="5" cols="70" name="synopsis"><?php echo $item["synopsis"]; ?></textarea><br>
+
+<tr><td>&nbsp;</td><td></td></tr>
 
 <?php
 	echo "<tr><td valign=\"top\"><b>Categories: </b></td><td>\n";
-	foreach (bib_get_cat_names() as $cat) {
-		echo "<input type=\"checkbox\" name=\"cats[]\" value=\"$cat\"";
-		if ($item["cats"]) if (in_array($cat, $item["cats"])) echo " checked";
-		echo "> $cat<br>\n";
+	echo "<table border=\"1\">\n";
+	foreach (bib_get_cat_groups() as $group) {
+		echo "<tr><td valign=\"top\">".$group.":</td><td>\n";
+		foreach (bib_get_cat_names_for_group($group) as $cat) {
+			echo "<input type=\"checkbox\" name=\"cats[]\" value=\"".$cat["name"]."\"";
+			if ($item["cats"]) if (in_array($cat["name"], $item["cats"])) echo " checked";
+			echo "> ".$cat["name"]."&nbsp;\n";
+		}
+		echo "</td></tr>\n";
 	}
+	echo "</table>\n";
 	echo "</td></tr>\n";
 ?>
 
@@ -66,5 +130,9 @@ foreach ($bib_item_fields as $field) {
 <br>
 
 <input type="submit" value="Submit changes">
+
+</form>
+
+</td></tr></table>
 
 <?php require 'footer.php'; ?>
